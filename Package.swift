ce83cd346391eef4ee242ebfe5de39d7b9ad7462
ahmedMaherE <asebaee@youxel.com>
2025-07-13 13:04:22 +0300

    package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "FSCalendar",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "FSCalendar",
+            targets: ["FSCalendar"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "FSCalendar",
+            path: "FSCalendar",
+            publicHeadersPath: "."
+        )
+    ]
+)
